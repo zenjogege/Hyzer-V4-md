cde821ce692748aba0f6e6662d92ff3d6a109eb4
zenjogege <99892635+zenjogege@users.noreply.github.com>
2022-04-01 22:48:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ second: 'numeric'
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-global.owner = ['17608914335'] // Put your number here
+global.owner = ['60137711356'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
